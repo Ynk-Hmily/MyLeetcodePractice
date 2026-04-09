9746d3c3ea96576610d8a69bee2a8456f2826ecc
Ynk-Hmily <2147047203@qq.com>
2026-04-09 15:00:02 +0800

    unfinished

---
+++ 394_decodeString/Solution.py
@@ -0,0 +1,17 @@
+"""
+LeetCode Problem: 394. decodeString
+"""
+
+#-----Solution----
+
+def Solution(self):
+
+
+#------Test----
+def test():
+	case=
+	for x in case:
+	    Solution(x)
+
+if __name__ == "__main__":
+    s = test()
